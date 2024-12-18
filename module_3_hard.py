af21b2a5859cfe266c3e5215e2b407cb1deda74f
Dmitrii Litvinov <k303Ghost@gmail.com>
2024-12-18 20:37:28 +0700

    module_3_hard.py

---
+++ module_3_hard.py
@@ -0,0 +1,26 @@
+def calculate_structure_sum(m):
+    global summa
+    for item in m:
+        if isinstance(item, (list, set, tuple)):
+            calculate_structure_sum(item)
+        elif isinstance(item, (dict)):
+            calculate_structure_sum(item.items())
+        elif isinstance(item, (int, float)):
+            summa += item
+        elif isinstance(item, str):
+            summa += len(item)
+    return summa
+
+data_structure = [
+[1, 2, 3],
+{'a': 4, 'b': 5},
+(6, {'cube': 7, 'drum': 8}),
+"Hello",
+((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+summa = 0
+
+result = calculate_structure_sum(data_structure)
+print(result)
+
