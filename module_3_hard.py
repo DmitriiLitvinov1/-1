8fa7af5f8305b41b1ce42954336d98ed9754dc48
Dmitrii Litvinov <k303Ghost@gmail.com>
2024-12-18 20:40:30 +0700

    module_3_hard.py

--- module_3_hard.py
+++ module_3_hard.py
@@ -11,16 +11,16 @@ def calculate_structure_sum(m):
             summa += len(item)
     return summa
 
+
 data_structure = [
-[1, 2, 3],
-{'a': 4, 'b': 5},
-(6, {'cube': 7, 'drum': 8}),
-"Hello",
-((), [{(2, 'Urban', ('Urban2', 35))}])
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
 summa = 0
 
 result = calculate_structure_sum(data_structure)
 print(result)
-
